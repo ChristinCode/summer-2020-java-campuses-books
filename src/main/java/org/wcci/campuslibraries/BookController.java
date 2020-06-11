676949e1e26357608e0e04c82bd7ac7d622ddd32
Ben Williams <DeanPBR@gmail.com>
2020-06-11 12:48:41 -0400

    Setting up workshop

--- src/main/java/org/wcci/campuslibraries/BookController.java
+++ src/main/java/org/wcci/campuslibraries/BookController.java
@@ -12,16 +12,16 @@ import java.util.Map;
 @Controller
 public class BookController {
 
-    Map<String, Book> books = new HashMap<>();
+    BookStorage bookStorage;
 
-    public BookController(){
-        books.put("Head First Java", new Book("Head First Java", "Kathy Sierra", "4440333044-2","Good book to learn Java."));
-        books.put("Test Driven Development by Example", new Book("Test Driven Development by Example", "Kent Beck", "44443333044-2","Good book to learn TDD."));
+
+    public BookController(BookStorage bookStorage){
+        this.bookStorage = bookStorage;
     }
 
     @RequestMapping("books/{bookTitle}")
     public String showSingleBook(@PathVariable String bookTitle, Model model){
-        model.addAttribute("book", books.get(bookTitle));
+        model.addAttribute("book", bookStorage.findBookByTitle(bookTitle));
         return "book-template";
     }
 
